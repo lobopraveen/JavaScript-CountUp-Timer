0b53efb5f9f6b2b2daddd0922bc11cb1ca14638e
lobo <lobopraveen@gmail.com>
2018-01-18 21:44:08 -0600

    fix for bug #1 and rewrite

--- countup.js
+++ countup.js
@@ -1,4 +1,3 @@
-
 /**********************************************************************************************
 # MIT License
 #
@@ -24,73 +23,64 @@
 #
 **********************************************************************************************/
 
-function CountUp(initDate,id){
-	this.beginDate = new Date(initDate);
-	this.numOfDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-	var currYear = (new Date()).getFullYear();
-	if ( (currYear % 4 == 0 && currYear % 100 != 0 ) || currYear % 400 == 0 ) {
-		this.numOfDays[1] = 29;
-	}
-	this.borrowed = 0, this.years = 0, this.months = 0, this.days = 0;
-	this.hours = 0, this.minutes = 0, this.seconds = 0;
-	this.calculate();
-	this.update(id);
+function CountUp(initDate, id){
+    this.beginDate = new Date(initDate);
+    this.countainer = document.getElementById(id);
+    this.numOfDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    this.borrowed = 0, this.years = 0, this.months = 0, this.days = 0;
+    this.hours = 0, this.minutes = 0, this.seconds = 0;
+    this.updateNumOfDays();
+    this.updateCounter();
+}
+
+CountUp.prototype.updateNumOfDays=function(){
+    var dateNow = new Date();
+    var currYear = dateNow.getFullYear();
+    if ( (currYear % 4 == 0 && currYear % 100 != 0 ) || currYear % 400 == 0 ) {
+        this.numOfDays[1] = 29;
+    }
+    var self = this;
+    setTimeout(function(){self.updateNumOfDays();}, (new Date((currYear+1), 1, 2) - dateNow));
 }
 
 CountUp.prototype.datePartDiff=function(then, now, MAX){
-	var temp = this.borrowed;
-	this.borrowed = 0;
-	var diff = now - then - temp;
-	if ( diff > -1 ) return diff;
-	this.borrowed = 1;
-	return (MAX + diff);
-}
-
-CountUp.prototype.formatTime=function(){
-	this.seconds = this.addLeadingZero(this.seconds);
-	this.minutes = this.addLeadingZero(this.minutes);
-	this.hours = this.addLeadingZero(this.hours);
-}
-
-CountUp.prototype.addLeadingZero=function(value){
-	return (value + "").length < 2 ? ("0" + value) : value;
+    var diff = now - then - this.borrowed;
+    this.borrowed = 0;
+    if ( diff > -1 ) return diff;
+    this.borrowed = 1;
+    return (MAX + diff);
 }
 
 CountUp.prototype.calculate=function(){
-	var currDate = new Date();
-	var prevDate = this.beginDate;
-	this.seconds = this.datePartDiff(prevDate.getSeconds(), currDate.getSeconds(), 60);
-	this.minutes = this.datePartDiff(prevDate.getMinutes(), currDate.getMinutes(), 60);
-	this.hours = this.datePartDiff(prevDate.getHours(), currDate.getHours(), 24);
-	this.days = this.datePartDiff(prevDate.getDate(), currDate.getDate(), this.numOfDays[currDate.getMonth()-1]);
-	this.months = this.datePartDiff(prevDate.getMonth(), currDate.getMonth(), 12);
-	this.years = this.datePartDiff(prevDate.getFullYear(), currDate.getFullYear(),0);
+    var currDate = new Date();
+    var prevDate = this.beginDate;
+    this.seconds = this.datePartDiff(prevDate.getSeconds(), currDate.getSeconds(), 60);
+    this.minutes = this.datePartDiff(prevDate.getMinutes(), currDate.getMinutes(), 60);
+    this.hours = this.datePartDiff(prevDate.getHours(), currDate.getHours(), 24);
+    this.days = this.datePartDiff(prevDate.getDate(), currDate.getDate(), this.numOfDays[currDate.getMonth()]);
+    this.months = this.datePartDiff(prevDate.getMonth(), currDate.getMonth(), 12);
+    this.years = this.datePartDiff(prevDate.getFullYear(), currDate.getFullYear(),0);
 }
 
-CountUp.prototype.update=function(id){
-	if ( ++this.seconds == 60 ) {
-		this.seconds = 0;
-		if ( ++this.minutes == 60 ) {
-			this.minutes = 0;
-			if ( ++this.hours == 24 ) {
-				this.hours = 0;
-				if ( ++this.days == this.numOfDays[(new Date()).getMonth()-1]){
-					this.days = 0;
-					if ( ++this.months == 12 ) {
-						this.months = 0;
-						this.years++;
-					}
-				}
-			}
-		}
-	}
-	this.formatTime();
-	var countainer = document.getElementById(id);
-	countainer.innerHTML ="<strong>" + this.years + "</strong> <small>years</small> <strong>" +
-		this.months + "</strong> <small>months</small><strong> " + this.days +
-		"</strong> <small>days</small> <strong>" + this.hours + "</strong> <small>hours</small> <strong>" +
-		this.minutes + "</strong> <small>minutes</small> <strong>" + this.seconds +
-		"</strong> <small>seconds</small>.";
-	var self=this;
-	setTimeout(function(){self.update(id);}, 1000);
+CountUp.prototype.addLeadingZero=function(value){
+    return value < 10 ? ("0" + value) : value;
+}
+
+CountUp.prototype.formatTime=function(){
+    this.seconds = this.addLeadingZero(this.seconds);
+    this.minutes = this.addLeadingZero(this.minutes);
+    this.hours = this.addLeadingZero(this.hours);
+}
+
+CountUp.prototype.updateCounter=function(){
+    this.calculate();
+    this.formatTime();
+    this.countainer.innerHTML ="<strong>" + this.years + "</strong> <small>" + (this.years == 1? "year" : "years") + "</small>" +
+        " <strong>" + this.months + "</strong> <small>" + (this.months == 1? "month" : "months") + "</small>" +
+        " <strong>" + this.days + "</strong> <small>" + (this.days == 1? "day" : "days") + "</small>" +
+        " <strong>" + this.hours + "</strong> <small>" + (this.hours == 1? "hour" : "hours") + "</small>" +
+        " <strong>" + this.minutes + "</strong> <small>" + (this.minutes == 1? "minute" : "minutes") + "</small>" +
+        " <strong>" + this.seconds + "</strong> <small>" + (this.seconds == 1? "second" : "seconds") + "</small>";
+    var self = this;
+    setTimeout(function(){self.updateCounter();}, 1000);
 }
